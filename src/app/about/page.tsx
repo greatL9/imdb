b268cb9fc1306ae4726dd577dd641a4d6f9c8bbe
Great Lucky <lgreat098@gmail.com>
2025-08-11 10:35:54 +0100

    add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,41 @@
 export default function AboutPage() {
-  return <div>About Page</div>;
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-500">About</h1>
+      <p>
+        with the latest TV series, MovieVerse aims to provide you with
+        comprehensive information, user-friendly navigation, and a seamless
+        browsing experience.We believe that movies have the power to inspire,
+        entertain, and bring people together. That is why we have built this app
+        using the latest web technologies to ensure a smooth and responsive
+        experience on any device. Whether you are browsing on your phone,
+        tablet, or desktop, you will enjoy quick access to everything you need.
+      </p>
+      <p>
+        Welcome to MovieVerse, your ultimate destination for exploring the
+        fascinating world of movies, TV shows, and entertainment all built with
+        modern web technologies! This app is a passion project designed to bring
+        the rich, dynamic experience of IMDb right to your fingertips. Whether
+        you are a casual moviegoer, an avid or someone who loves keeping who
+        enjoy exploring the vast world of films and TV shows. Here, you can find
+        detailed information about countless titles, from timeless classics to
+        the latest releases. Our goal is to create an engaging and easy-to-use
+        environment where users can discover new favorites, learn about their
+        favorite actors and directors, and stay updated with trending
+        entertainment news.
+      </p>
+
+      <p>
+        Welcome to our movie app! This platform is dedicated to all movie lovers
+        This project was created as a way to combine passion for cinema with
+        modern development practices. Behind the scenes, we utilize powerful
+        tools and APIs to gather accurate data and present it in a clean and
+        user-friendly interface. Our vision is to continuously improve and add
+        exciting features like personalized, ratings, and community reviews to
+        make your experience even better. Thank you for visiting and being part
+        of this journey. We hope you enjoy using the app as much as we enjoyed
+        building it!
+      </p>
+    </div>
+  );
 }
