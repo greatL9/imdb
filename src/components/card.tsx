1b0dcfa5ee00abc5f25724b97941f42c08483d19
Great Lucky <lgreat098@gmail.com>
2025-09-02 23:11:49 +0100

    add card component

---
+++ src/components/card.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiThumbsUp } from "react-icons/fi";
+
+interface Result {
+  id: string | number;
+  original_title: string;
+  backdrop_path?: string;
+  poster_path?: string;
+  overview: string;
+  title?: string;
+  name?: string;
+  release_date?: string;
+  first_air_date?: string;
+  vote_count?: number;
+}
+
+export default function Card({ result }: { result: Result }) {
+  return (
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={1000}
+          height={600}
+          alt="image not available"
+          priority={true}
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200 w-full h-auto"
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+        />
+      </Link>
+      <div className="p-2 text-md">
+        <p className="line-clamp-2 leading-relaxed">{result.overview}</p>
+        <h2 className="truncate text-lg font-bold">
+          {result.title || result.name}
+        </h2>
+        <p className="flex items-center">
+          {result.release_date || result.first_air_date}
+          <FiThumbsUp className="h-5 mr-1 ml-3" />
+          {result.vote_count}
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,15 +1,18 @@
-interface Result {
+import Card from "./card";
+
+type Result = {
   id: string | number;
   original_title: string;
-}
+  backdrop_path?: string;
+  poster_path?: string;
+  overview: string;
+};
 
 export default function Results({ results }: { results: Result[] }) {
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {results &&
-        results.map((result) => (
-          <div key={result.id}>{result.original_title}</div>
-        ))}
+        results.map((result) => <Card key={result.id} result={result} />)}
     </div>
   );
 }
