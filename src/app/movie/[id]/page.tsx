6724e2600c328e585c88981925dfc69b01e73d8f
Great Lucky <lgreat098@gmail.com>
2025-09-03 23:52:08 +0100

    add movie page

---
+++ src/app/movie/[id]/page.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+const getMovie = async (movieId: string) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+  );
+  return await res.json();
+};
+
+export default async function Movie({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const movie = await getMovie(id);
+  return (
+    <div className="w-full">
+      <div className="p-4 md:pt-8 flex flex-col  md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
+        <Image
+          src={`https://image.tmdb.org/t/p/original${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          width={500}
+          height={300}
+          alt="Movie poster"
+          priority
+          className="rounded-t-lg"
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+        />
+        <div className="text-lg mb-3 font-bold p-2">
+          <h2>{movie.title || movie.name}</h2>
+          <p className="text-lg mb-3">
+            <span className="font-semibold mr-1">Overview:</span>
+            {movie.overview}
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Date released:</span>
+            {movie.release_date || movie.first_air_date}
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Rating:</span>
+            {movie.vote_count}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
